333699ce70c55d1d7dc4a5c5ecf8e6b4a6fdccd3
juan david r <iamjuandavid21@gmail.com>
2023-08-30 22:39:20 -0500

    done

---
+++ src/pages/TotalPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AvatarNavbar from "../components/global/AvatarNavbar";
+import Footer from "../components/global/Footer";
+import TotalComponent from "../components/admin/TotalComponent";
+
+function TotalPage() {
+  return (
+    <div>
+      <AvatarNavbar />
+      <TotalComponent/>
+      <Footer />
+    </div>
+  );
+}
+
+export default TotalPage;
