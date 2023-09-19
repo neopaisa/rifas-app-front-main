cd1ba235291ec8cc960a3a4e3870eacae68741e0
Jose Luis <joseluisgm1998@gmail.com>
2023-09-19 13:20:40 -0500

    se cambio los datos del footer por datos reales

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
           <img src={logoBlanco} alt="" className="bg-red w-32" />
         </div>
         <div className="flex justify-center"> 
-          <a href="https://www.instagram.com/rifacasa_torovalle/" rel="noreferrer" className="mr-6 text-white dark:text-neutral-200" target="_blank">
+          <a href="https://instagram.com/rifacasa_torovalle?igshid=NzZhOTFlYzFmZQ==" rel="noreferrer" className="mr-6 text-white dark:text-neutral-200" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4"
@@ -105,6 +105,7 @@ function Footer() {
               </svg>
               Toro Valle Del Cauca
             </p>
+
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -112,10 +113,13 @@ function Footer() {
                 fill="currentColor"
                 className="mr-3 h-5 w-5"
               >
-                <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
-                <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+                <path
+                  fillRule="evenodd"
+                  d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                  clipRule="evenodd"
+                />
               </svg>
-              rifatoro@gmail.com
+              3170507261
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
@@ -130,7 +134,7 @@ function Footer() {
                   clipRule="evenodd"
                 />
               </svg>
-              3216549870
+              3022607814
             </p>
           </div>
         </div>
